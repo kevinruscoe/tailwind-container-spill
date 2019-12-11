861e6f9653abbd12683eca02cddc94728071d9c1
Kevin Ruscoe <hello@kevinruscoe.me>
2019-12-11 19:47:38 +0000

    Add file

---
+++ container.js
@@ -0,0 +1,26 @@
+module.exports = function({ addUtilities, theme }) {
+    const screens = theme('screens', {})
+
+    let containers = {
+        '.container--spill': {
+            width: '100vw'
+        }
+    };
+  
+    for (var prop in screens) {
+        if (Object.prototype.hasOwnProperty.call(screens, prop)) {
+            let width = screens[prop];
+            let halfWidth = width.replace(/[^0-9\.]+/g, '') / 2;
+
+            containers[`@media (min-width: ${width})`] = {
+                '.container--spill': {
+                    width: '100vw',
+                    border: '0 solid transparent',
+                    borderWidth: `0 calc(50vw - ${halfWidth}px)`
+                }
+            };
+        }
+    }
+  
+    addUtilities(containers, ['responsive']);
+}
